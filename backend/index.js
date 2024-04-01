e8e2e6fe01c73bd864b0927c9dfb2e2494db40ba
mahnooratif12 <X00176345@mytudublin.ie>
2024-04-01 12:57:40 +0100

    updated.

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,29 @@
+import express from 'express'; 
 import * as database from './database.js'; 
 
+const PORT = 3060;  
+
 database.connect(); 
+
+const app = express(); 
+
+app.post('/upload', (req, res) => {
+    const file = req.file;
+    if (!file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    // Save the filename to the database
+    const filename = file.filename;
+
+    // parse data from exel file 
+    // write sql queries to insert data to database 
+    
+    console.log('Filename saved to database successfully');
+    res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
