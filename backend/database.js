bd8b77f52ac525bd6476fe20819b6b5603e4fe72
Mahnoor Atif <X00176345@myTUDublin.ie>
2024-04-05 12:14:00 +0100

    updated create table scripts

--- backend/database.js
+++ backend/database.js
@@ -35,44 +35,12 @@ const CREATE_CLINICIAN_TABLE_QUERY ="CREATE TABLE IF NOT EXISTS `clinician` (" +
     "`source_code` varchar(128) NOT NULL,"+
     "`source_class` varchar(128) NOT NULL,"+
     "PRIMARY KEY (`id`))";
-const CREATE_Reference_Test_Result_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS `age-reference-test` ("+
-    "`id` int(11) NOT NULL,"+
-    "`age_id` int(11) NOT NULL,"+
-    "`reference_id` varchar(128) NOT NULL,"+
-    "PRIMARY KEY (`id`)) ";
-const CREATE_RESULT_QUERY = "CREATE TABLE IF NOT EXISTS `result` ("+
-    "`id` int(11) NOT NULL,"+
-    "`test_result` varchar(255) NOT NULL,"+
-    "PRIMARY KEY (`id`))"; 
-const CREATE_TEST_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS `test` ("+
-    "`id` int(11) NOT NULL,"+
-    "`age_reference_id` int(11) NOT NULL,"+
-    "`TCL` varchar(45) NOT NULL,"+
-    "`TFC` varchar(45) NOT NULL,"+
-    "`testnames` varchar(128) NOT NULL,"+
-    "`unit` varchar(45) NOT NULL,"+
-    "`ageReference` varchar(128) NOT NULL,"+
-    "PRIMARY KEY (`id`))";
-const CREATE_REFERENCE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS `reference` ("+
-    "`id` int(11) NOT NULL,"+
-    "`test_id` int(11) NOT NULL,"+
-    "`age_ref` int(11) NOT NULL,"+
-    "`ref_range` decimal(10,0) NOT NULL,"+
-    "`flaglimitlow` decimal(10,0) NOT NULL,"+
-    "`flaglimithigh` decimal(10,0) NOT NULL,"+
-    "`alertlimitlow` decimal(10,0) NOT NULL,"+
-    "`alertlimithigh` decimal(10,0) NOT NULL,"+
-    "PRIMARY KEY (`id`)) ";
-const CREATE_Age_Reference_Test_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS `age-reference-test` ("+
-    "`id` int(11) NOT NULL,"+
-    "`age_id` int(11) NOT NULL,"+
-    "`reference_id` varchar(128) NOT NULL,"+
-    "PRIMARY KEY (`id`))";
-const CREATE_Age_Result_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS `age` ("+
-    "`id` int(11) NOT NULL,"+
-    "`value` int(11) NOT NULL,"+
-    "`period` varchar(128) NOT NULL,"+
-    "PRIMARY KEY (`id`))";
+// const CREATE_Reference_Test_Result_TABLE_QUERY = "CREATE TABLE 'tuh'. 'reference_test_result'( `Request_Test_Result_ID` INT NOT NULL,`Test_Request_ID` INT NOT NULL,`Test_ID` INT NOT NULL,`Request_Test_Resultcol` VARCHAR(45) NOT NULL,PRIMARY KEY (`Request_Test_Result_ID`))";
+// const CREATE_RESULT_QUERY = "CREATE TABLE `tuh`.`result` (`Result_ID` INT NOT NULL,`Reference_Test_Result_ID` INT NOT NULL,`Test_Result` VARCHAR(255) NOT NULL,PRIMARY KEY (`Result_ID`));"; 
+// const CREATE_TEST_TABLE_QUERY = "CREATE TABLE 'tuh'.'test'( `Test_ID` INT NOT NULL,`Test_Req_ID` INT NOT NULL,`TCL` VARCHAR(45) NOT NULL,`TFC` VARCHAR(45) NOT NULL,`testnames` VARCHAR(45) NOT NULL,`unit` VARCHAR(45) NOT NULL,PRIMARY KEY (`Test_ID`))";
+// const CREATE_REFERENCE_TABLE_QUERY = "CREATE TABLE `tuh`.`reference` (`Reference_Id` INT NOT NULL,`Test_Id` INT NOT NULL,`age_ref` INT NOT NULL,`ref_range` DECIMAL NOT NULL,`flaglimitlow` DECIMAL NOT NULL,`flaglimithigh` DECIMAL NOT NULL,`Alertlimitlow` DECIMAL NOT NULL,`Alertlimithigh` DECIMAL NOT NULL,`Referencecol` VARCHAR(45) NOT NULL,PRIMARY KEY (`Reference_Id`))";
+// const CREATE_Age_Reference_Test_TABLE_QUERY = "CREATE TABLE Age_Reference_Test()";
+// const CREATE_Age_Result_TABLE_QUERY = "CREATE TABLE `tuh`.`age_result` ( `Age_result_id` INT NOT NULL,`Reference_id` INT NOT NULL,`Age` INT NOT NULL,`ref` VARCHAR(45) NOT NULL,PRIMARY KEY (`Age_result_id`))";
 
 // Add queries to create remaining tables 
 
