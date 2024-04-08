f9c71a23f5c233d64eaa4073fe1924c82c1e661e
Mahnoor Atif <X00176345@myTUDublin.ie>
2024-04-08 09:51:27 +0100

    Visualization function still have few errors

--- website/src/Visualization.js
+++ website/src/Visualization.js
@@ -1,34 +1,40 @@
-function Visualization() {
-    return(
-        <div>
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-                        <div class="container-fluid">
-                        <a class="navbar-brand" href="./homepage"><img src="TUH logo.jpg" alt="TUH Logo" class="img"/> </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse justify-content-center" id="mynavbar">
-                            <ul class="navbar-nav me-auto justify-content-center">
-                            <li class="nav-item justify-content-center ">
-                                <a class="nav-link justify-content-center " href="./Visualization">Visualization</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link justify-content-center " href="./Importdata">Import Data</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link justify-content-center " href="./Help">Help</a>
-                            </li>
-                            </ul>
-                            <form class="d-flex">
-                            <input class="form-control me-2" type="text" placeholder="Search"/>
-                            <button class="btn btn-primary" type="button">Search</button>
-                            </form>
-                        </div>
-                        </div>
-                    </nav>
-                    Visualization page 
-        </div>
-    )
+import { Chart } from 'chart.js';
+
+export function createLineChart() {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  // rest of the code
 }
 
-export default Visualization; 
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+        data: [0, 10, 5, 2, 20, 30, 45, 40, 50, 45, 35, 50],
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+
+function Visualization() {
+  return (
+    <div>
+      <h1>Hello Visualization</h1>
+      <canvas id="myChart" width="800" height="400"></canvas>
+    </div>
+  );
+}
+
+export default Visualization;
