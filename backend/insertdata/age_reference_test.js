4800acce22626751d1b5493681a90c7020395bbb
Mahnoor Atif <X00176345@myTUDublin.ie>
2024-05-28 17:44:03 +0100

    all Insertdata files added

---
+++ backend/insertdata/age_reference_test.js
@@ -0,0 +1,42 @@
+import mysql from 'mysql';
+
+export function insert(data) { 
+    // Configure MySQL connection
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'tuh'
+    }); 
+
+    console.log("data: "+data); 
+
+    //Establish MySQL connection
+    connection.connect(function(err) {
+        if (err) 
+            throw err
+        else {
+            console.log('Connected to MySQL');
+            for (var i=0; i<data.length; i++) { 
+                console.log(data[i]); 
+                var query = getPatientInsertQuery(data[i]); 
+                console.log(query); 
+                connection.query(query, function(err, result){ 
+                    if(err) { 
+                        console.log(err); 
+                    } else { 
+                        console.log("Data entered! "); 
+                    }
+                }); 
+            }
+            
+        }
+    });
+}
+
+function getPatientInsertQuery(dataRecord) { 
+    return `INSERT INTO patient `+
+    `(age_id, reference_id) `+ 
+    `VALUES (${dataRecord['age_id']}, ${dataRecord['reference_id']},)`; 
+}
+
