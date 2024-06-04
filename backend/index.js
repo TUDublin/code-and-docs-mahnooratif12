29a3a941a9b614dc3a64c76d6e006f204bdb710b
Mahnoor Atif <X00176345@myTUDublin.ie>
2024-06-04 14:47:25 +0100

    Test data entered git add backend/

--- backend/index.js
+++ backend/index.js
@@ -159,7 +159,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mmol/L"
             
             
-        },  { 
+        }, 
+        { 
             "Test_Req_ID": 2,
             "TCL":"RP",
             "TFC":"Urea", 
@@ -167,7 +168,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mmol/L"
             
             
-        },  { 
+        },  
+        { 
             "Test_Req_ID": 2,
             "TCL":"RP",
             "TFC":"Crea", 
@@ -175,7 +177,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"umol/L"
             
             
-        },  { 
+        },  
+        { 
             "Test_Req_ID": 3,
             "TCL":"CRP",
             "TFC":"CRP", 
@@ -183,7 +186,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mg/L"
             
             
-        },  { 
+        },  
+        { 
             "Test_Req_ID": 4,
             "TCL":"FLP",
             "TFC":"Chol", 
@@ -191,7 +195,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mmol/L"
             
             
-        },  { 
+        },  
+        { 
             "Test_Req_ID": 4,
             "TCL":"FLP",
             "TFC":"TRIG", 
@@ -199,7 +204,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mmol/L"
             
             
-        },  { 
+        }, 
+         { 
             "Test_Req_ID": 4,
             "TCL":"FLP",
             "TFC":"LDL", 
@@ -207,7 +213,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mmol/L"
             
             
-        },  { 
+        }, 
+         { 
             "Test_Req_ID": 4,
             "TCL":"FLP",
             "TFC":"HDL", 
@@ -215,7 +222,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mmol/L"
             
             
-        },  { 
+        },  
+        { 
             "Test_Req_ID": 4,
             "TCL":"FLP",
             "TFC":"nonH", 
@@ -223,7 +231,8 @@ app.post('/upload/test', (req, res) => {
             "unit":"mmol/L"
             
             
-        },  { 
+        },  
+        { 
             "Test_Req_ID": 5,
             "TCL":"TFT",
             "TFC":"FT4", 
@@ -232,6 +241,152 @@ app.post('/upload/test', (req, res) => {
             
             
         },
+        { 
+            "Test_Req_ID": 5,
+            "TCL":"TFT",
+            "TFC":"TSH", 
+            "testnames":"Thyroid Stimulating Hormone", 
+            "unit":"mU/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 6,
+            "TCL":"LP",
+            "TFC":"PROT", 
+            "testnames":"Total Protein", 
+            "unit":"g/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 6,
+            "TCL":"LP",
+            "TFC":"ALB", 
+            "testnames":"Albumin", 
+            "unit":"g/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 6,
+            "TCL":"LP",
+            "TFC":"TBIL", 
+            "testnames":"Total Bilirubin", 
+            "unit":"umol/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 6,
+            "TCL":"LP",
+            "TFC":"ALKP", 
+            "testnames":"Akaline Phosphatse", 
+            "unit":"IU/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 6,
+            "TCL":"LP",
+            "TFC":"ALT", 
+            "testnames":"Alanine Aminotransferase", 
+            "unit":"IU/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 6,
+            "TCL":"LP",
+            "TFC":"GGT", 
+            "testnames":"Gmma-Glutamyl transferase", 
+            "unit":"IU/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 7,
+            "TCL":"BP",
+            "TFC":"PROT", 
+            "testnames":"Total Protein", 
+            "unit":"g/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 7,
+            "TCL":"BP",
+            "TFC":"ALB", 
+            "testnames":"Albumin", 
+            "unit":"g/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 7,
+            "TCL":"BP",
+            "TFC":"Ca", 
+            "testnames":"Calcium", 
+            "unit":"pmol/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 7,
+            "TCL":"BP",
+            "TFC":"CCA", 
+            "testnames":"Corrected Calcium (calculation)", 
+            "unit":"mmol/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 7,
+            "TCL":"BP",
+            "TFC":"Phos", 
+            "testnames":"Phosphate", 
+            "unit":"mmol/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 8,
+            "TCL":"MG",
+            "TFC":"MG", 
+            "testnames":"Magnesium", 
+            "unit":"mmol/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 9,
+            "TCL":"VITD",
+            "TFC":"VID2", 
+            "testnames":"Vitamin D", 
+            "unit":"nmol/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 10,
+            "TCL":"TNX1",
+            "TFC":"TNHS", 
+            "testnames":"Troponin", 
+            "unit":"ng/L"
+            
+            
+        },
+        { 
+            "Test_Req_ID": 11,
+            "TCL":"BNPL",
+            "TFC":"", 
+            "testnames":"NT pro BNP", 
+            "unit":"pg/L"
+            
+            
+        },
+        
+
     ]; 
 
 
