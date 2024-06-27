0efc116661165271916ccf0c904eac1192d29677
Mahnoor Atif <X00176345@myTUDublin.ie>
2024-06-27 18:28:19 +0100

    trying to resolve issues

--- website/src/LoginValidation.js
+++ website/src/LoginValidation.js
@@ -1,28 +1,21 @@
 function Validation(values) {
-    let error = {}
-    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
-    
-    if(values.email === "") {
-        error.email = "Name should not be empty"
+    let errors = {};
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+  
+    if (!values.email) {
+      errors.email = "Email should not be empty";
+    } else if (!emailPattern.test(values.email)) {
+      errors.email = "Email is invalid";
     }
-    else if(!email_pattern.test(values.email)) {
-        error.email = "Email Didn't match"
+  
+    if (!values.password) {
+      errors.password = "Password should not be empty";
+    } else if (!passwordPattern.test(values.password)) {
+      errors.password = "Password must contain at least 8 characters, including uppercase, lowercase, and a number";
     }
-    else 
-    {
-        error.email = ""
-    }
-    if(values.password === "") {
-        error.password = "Password should not be empty"
-    }
-    else if(!password_pattern.test(values.password)) {
-        error.password = "Password didn't match"
-    }
-    else {
-        error.password = ""
-    }
-    return error ;
-}
-
-export default Validation;
+  
+    return errors;
+  }
+  
+  export default Validation;
