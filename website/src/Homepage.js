ae452e4ab00a60396de55bf8ae8e284acb263e42
Mahnoor Atif <X00176345@myTUDublin.ie>
2024-07-15 12:19:43 +0100

    data styling in Homepage

--- website/src/Homepage.js
+++ website/src/Homepage.js
@@ -70,19 +70,19 @@ function Homepage() {
                 </p>
             </div>
             <div className='card'> 
-                <DataTable value={patients} showGridlines stripedRows paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
-                    <Column field="lab_no" header="Lab #"></Column>
-                    <Column field="ocs_no" header="OCS #"></Column>
-                    <Column field="mrn" header="MRN"></Column>
-                    <Column field="forename" header="Forename"></Column>
-                    <Column field="surname" header="Surname"></Column>
-                    <Column field="dob" header="dob"></Column>
-                    <Column field="gender" header="gender"></Column>
-                    <Column field="age" header="age"></Column>
-                    <Column field="address1" header="address1"></Column>
-                    <Column field="address2" header="address2"></Column>
-                    <Column field="address3" header="address3"></Column>
-                    <Column field="phone_no" header="phone_no"></Column>
+                <DataTable value={patients} removableSort sortField="mrn" sortOrder={-1} sortMode="multiple" showGridlines stripedRows paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
+                    <Column field="lab_no" header="Lab" sortable style={{ width: '25%' }}></Column>
+                    <Column field="ocs_no" header="OCS" sortable style={{ width: '25%' }}></Column>
+                    <Column field="mrn" header="MRN" sortable style={{ width: '25%' }}></Column>
+                    <Column field="forename" header="Forename" sortable style={{ width: '25%' }}></Column>
+                    <Column field="surname" header="Surname" sortable style={{ width: '25%' }}></Column>
+                    <Column field="dob" header="dob" sortable style={{ width: '25%' }}></Column>
+                    <Column field="gender" header="gender" sortable style={{ width: '25%' }}></Column>
+                    <Column field="age" header="age" sortable style={{ width: '25%' }}></Column>
+                    <Column field="address1" header="address1" sortable style={{ width: '25%' }}></Column>
+                    <Column field="address2" header="address2" sortable style={{ width: '25%' }}></Column>
+                    <Column field="address3" header="address3" sortable style={{ width: '25%' }}></Column>
+                    <Column field="phone_no" header="phone_no" sortable style={{ width: '25%' }}></Column>
                 </DataTable>
             </div>    
             
